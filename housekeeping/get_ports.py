22fdc2307c79238f0e3faa9bb086494aa99cc889
gsfcprime <joseph.m.durbak@nasa.gov>
2023-12-07 16:06:14 -0500

    added convenience function for setting up other software

---
+++ housekeeping/get_ports.py
@@ -0,0 +1,25 @@
+import json
+
+from serial.tools.list_ports import comports
+
+with open('devices.json') as f:
+    devices = json.load(f)
+
+comports = comports()
+
+devices_flattened = {}
+
+for device_type, device_type_dict in devices.items():
+    for device_name, device_list in device_type_dict.items():
+        for device_number, device_dict in enumerate(device_list):
+            devices_flattened[f'{device_type}_{device_name}_{device_number}'] = device_dict
+
+# print(devices_flattened)
+
+comport_dict = {}
+for port in comports:
+    comport_dict[port.serial_number] = port.device
+
+for device_name, device_dict in devices_flattened.items():
+    port = comport_dict[device_dict['serial_number']]
+    print(device_name, port)
